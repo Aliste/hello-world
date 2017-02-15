cdda3720a019fa48e9ccb3688b6f4c484ceb9d26
Михаил Иванов <mihailivanov@mac-mini-mihail.local>
2017-02-15 16:15:32 +0300

    Initial Commit

---
+++ helo-world/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  helo-world
+//
+//  Created by Михаил Иванов on 15.02.17.
+//  Copyright © 2017 Михаил Иванов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
